e516de243d4daa9d3ee00a9db420e0e28d979359
pranathipu <pranathipu18@gmail.com>
2026-04-15 13:54:36 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,9 +1,9 @@
 import streamlit as st
 from groq import Groq
 
-st.set_page_config("PragyanAI Content Generator", layout="wide")
-st.title("PragyanAI – Content Generator")
-st.image("PragyanAI_Transperent.jpg")
+st.set_page_config("Ferrari", layout="wide")
+st.title("Ferrari – Content Generator")
+st.image("Ferrari.jpg")
 
 # Get GROQ API Key
 client = Groq(api_key=st.secrets["GROQ_API_KEY"])
